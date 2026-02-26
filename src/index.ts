0f8ae8d7a106fb34d0d945dfee3ad8a25f405916
Mardhav-B <batchu.sai@bounteous.com>
2026-02-26 15:55:51 +0530

    adding route

--- src/index.ts
+++ src/index.ts
@@ -9,29 +9,24 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running ");
+});
+
 app.use("/todos", todoRoutes);
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
-const mongooseOptions = {
-  serverSelectionTimeoutMS: 5000,
-  socketTimeoutMS: 5000,
-};
-
 mongoose
-  .connect(process.env.MONGO_URI as string, mongooseOptions)
+  .connect(process.env.MONGO_URI as string)
   .then(() => {
     console.log("MongoDB Connected");
-    startServer();
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch((err) => {
-    console.error("DB Connection Error:", err.message);
-    process.exit(1);
+    console.error("DB Error:", err.message);
   });
-
-function startServer() {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
